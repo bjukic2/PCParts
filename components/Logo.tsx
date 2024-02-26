a142055825170da374a9c99ad170becd9fd216b1
bjukic2 <brunojukic68@gmail.com>
2024-02-26 22:30:56 +0100

    Added clickability to logo icon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import { FC } from "react";
 
 import { TbDevicesPc } from "react-icons/tb";
 
 const Logo: FC = () => (
     <div className="flex items-center justify-between max-w-min gap-2 text-gray-500">
-        <TbDevicesPc size={70}/>
+        <Link href="/"><TbDevicesPc size={70}/></Link>
         <span className="font-roboto-condensed font-bold text-3xl whitespace-nowrap text-gray-500">
-            pcparts
+            <Link href="/">pcparts</Link>
         </span>
     </div>
 );
