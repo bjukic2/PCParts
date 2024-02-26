1862737c63442894c4189ecd9fbaa82c9a083c68
bjukic2 <brunojukic68@gmail.com>
2024-02-26 22:33:46 +0100

    Updated clickability of icon logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,12 +4,14 @@ import { FC } from "react";
 import { TbDevicesPc } from "react-icons/tb";
 
 const Logo: FC = () => (
-    <div className="flex items-center justify-between max-w-min gap-2 text-gray-500">
-        <Link href="/"><TbDevicesPc size={70}/></Link>
-        <span className="font-roboto-condensed font-bold text-3xl whitespace-nowrap text-gray-500">
-            <Link href="/">pcparts</Link>
-        </span>
-    </div>
+    <Link href="/">
+        <div className="flex items-center justify-between max-w-min gap-2 text-gray-500">
+            <TbDevicesPc size={70} />
+            <span className="font-roboto-condensed font-bold text-3xl whitespace-nowrap text-gray-500">
+                pcparts
+            </span>
+        </div>
+    </Link>
 );
 
 export default Logo;
